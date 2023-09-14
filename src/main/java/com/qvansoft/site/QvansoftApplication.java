fcd089f5fe6cf43c0a42799628ed635f71193df0
Abdiel Matos Nieto <hackerabdi@gmail.com>
2023-09-14 17:53:38 +0300

    First changes

---
+++ src/main/java/com/qvansoft/site/QvansoftApplication.java
@@ -0,0 +1,13 @@
+package com.qvansoft.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QvansoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QvansoftApplication.class, args);
+	}
+
+}
